7fc57b117f68b153a5fe930fd35318c0ed35fac0
CastaChick <casta46chick@gmail.com>
2019-12-02 22:28:21 +0900

    細かい変更

--- camael/nn/metrix.py
+++ camael/nn/metrix.py
@@ -11,7 +11,7 @@ def accuracy(y, y_pred):
         正解ラベル
 
     y_pred: array
-        予測値
+        予測ラベル
 
     Returns
     -------
@@ -21,3 +21,23 @@ def accuracy(y, y_pred):
     y = np.argmax(y, axis=1)
 
     return np.sum(y_pred == y) / y_pred.shape[0] * 100
+
+
+def mean_squre_error(y, y_pred):
+    """
+    回帰問題で平均二乗誤差を算出する
+
+    Parameters
+    ----------
+    y: array
+        正解値
+
+    y_pred: array
+        予測値
+
+    Returns
+    -------
+    loss: float
+        平均二乗誤差
+    """
+    return np.sum((y - y_pred)**2) / y.shape[0]
